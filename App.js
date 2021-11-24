473ea6cbedecc2486de811189bc4a4c901f0a39b
Ghani <razwanulghani@gmail.com>
2021-11-24 23:00:23 +0600

    change in flexbox

--- App.js
+++ App.js
@@ -9,40 +9,64 @@ import {
 
 export default function App() {
 
-  const buttonChangeName = () => {
-    if(name == 'Istiak'){
-      setName('Ghani')
-    }else{
-      setName('Istiak')
-    }
-    setCounter(counter + 1)
-  }
-
-  const [name, setName] = useState('Mash')
-
-  const [counter, setCounter] = useState(0)
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{name}</Text>
-      <Text style={styles.text}>{counter}</Text>
-      <Button 
-      onPress={() => buttonChangeName()}
-        title="Change Name"
-      />
+    <View style={{
+      flex : 1,
+      backgroundColor: '#000'
+    }}>
+      <View style={styles.container1}>
+        <View style={styles.view1}>
+          <Text style={styles.text1}>1</Text>
+        </View>
+        <View style={styles.view2}>
+          <Text style={styles.text1}>2</Text>
+        </View>
+        <View style={styles.view3}>
+          <Text style={styles.text1}>3</Text>
+        </View>
+        <View style={styles.view4}>
+          <Text style={styles.text1}>4</Text>
+        </View>
+      </View>
     </View>
+      
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container1: {
+    flex:1,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    flexDirection : 'row',
   },
-  text : {
+  text1 : {
     fontSize : 70,
     color : '#fff'
+  },
+  view1 :{
+    flex : 1,
+    backgroundColor : 'red',
+    alignItems : 'center',
+    justifyContent : 'center'
+  },
+  view2 :{
+    flex : 2,
+    backgroundColor : 'green',
+    alignItems : 'center',
+    justifyContent : 'center'
+  },
+  view3 :{
+    flex: 3,
+    backgroundColor : 'blue',
+    alignItems : 'center',
+    justifyContent : 'center'
+  },
+  view4 :{
+    flex : 1,
+    backgroundColor : '#000',
+    alignItems : 'center',
+    justifyContent : 'center'
   }
 });
